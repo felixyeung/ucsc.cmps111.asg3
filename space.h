d14e87654b124962a24e2edf47d0fa1a1f01d462
Quentin Nolan Rivers <qrivers@unix2.ic.ucsc.edu>
2012-05-28 03:45:36 -0700

    Struct space now agrees with the design, thanks to the magic of copy-pasting.

--- space.h
+++ space.h
@@ -2,23 +2,39 @@
 #define SPACE_H
 
 struct space {
-   char type; //’b’ forbuddy, ‘s’ for slab, ‘f’ for free list
+   char type; //’b’ for buddy, ‘s’ for slab, ‘f’ for free list
 
    int handle; //handle number
 
    void *head; //pointer to start of mem space
 
-   void *end; // pointer to end of mem space
+   void *end; // pointer to the last byte of mem space
 
    int size; //size of space
 
-   void *bitmaps; /*pointer to an array of pointers to bitmaps,used in Buddy alloc*/
+   void *bitmaps; /* pointer to an array of pointers to bitmaps, used in
+                   * Buddy and slab alloc
+                   */
 
-   int numBitmaps; /*how many bitmaps are we using? used in Buddy alloc*/
+   int numBitmaps; /* how many bitmaps are we using? used in Buddy and
+                    * slab alloc
+                    */
 
-   void *first_free; //used in SLAB and Free list
+   int listType; //Used for freelist
 
-   int min_page_size; //min size of space
+   void *firstFree; //used in SLAB  and Free list
+
+   void *nextFree; //used in next fit
+
+   int minPageSize; //min size of space
+
+   int* sizeArray; // Array of object sizes to be allocated (for slab)
+
+   int numSlabs; // Size of the slab array
+
+   int* slabs; // Array corresponding to the set of slabs
+
+   int slabSize; // Size of a slab
 };
 
 #endif
