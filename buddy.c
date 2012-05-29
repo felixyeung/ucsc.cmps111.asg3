14391fd8c74c08f2557aaf36a32942ff91639b65
Quentin Nolan Rivers <qrivers@ucsc.edu>
2012-05-28 18:15:44 -0700

    All three allocators *compile*. No data on running yet. Buddy is still brokened.

--- buddy.c
+++ buddy.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "space.h"
 
@@ -8,17 +9,17 @@ int isPowerOfTwo (unsigned int x) {
  return (x == 1);
 }
 
-void markDown(long n_bytes, void* maps, int mapIdx, int blockIdx, int blockSize) {
+void markDown(long n_bytes, char** maps, int mapIdx, int blockIdx, int blockSize) {
 	if (n_bytes <= blockSize) {
 		maps[mapIdx][blockIdx] = 1;
 		markDown(n_bytes, maps, mapIdx + 1, blockIdx * 2, blockSize / 2);
 	}
 }
 
-void* allocAndMark(long n_bytes, void* maps, int mapIdx, int blockIdx, int blockSize) {
-	int mapSize = 2 ^ mapIdx + 1;
+void* allocAndMark(struct space* s, long n_bytes, char** maps, int mapIdx, int blockIdx, int blockSize) {
+	int mapSize = 1 << (mapIdx + 1);
 	int j;
-	for (j = blockIndx; j < mapSize; j++) {
+	for (j = blockIdx; j < mapSize; j++) {
 		/* we try to allocate at the current level, figure out which buddy we are*/
 		int buddy;
 		
@@ -36,34 +37,35 @@ void* allocAndMark(long n_bytes, void* maps, int mapIdx, int blockIdx, int block
 			
 			/* WE need to travel back down 
 			*/
-			markDown(n_byte, maps, mapIdx + 1, j * 2, blockSize / 2);
+			markDown(n_bytes, maps, mapIdx + 1, j * 2, blockSize / 2);
 			
 			return slot;
 			
 		}
 		else 
-			allocAndMark(n_bytes, maps, mapIdx - 1, j / 2, blockSize * 2);
+			return allocAndMark(s, n_bytes, maps, mapIdx - 1, j / 2, blockSize * 2);
 	}
+	return NULL;
 }
 
-void freeAndMergeBuddies(void* maps, int mapIdx, int b1) {	
+void freeAndMergeBuddies(char** maps, int mapIdx, int b1) {	
 	/*first free the first buddy */
 	
-	map[mapIdx][b1] = 0;
+	maps[mapIdx][b1] = 0;
 	
 	/* then, let's look at our buddy */
 	int b2;
-	if (k % 2 == 0) {
-		b2 = k + 1;
+	if (b1 % 2 == 0) {
+		b2 = b1 + 1;
 	}
 	else {
-		b2 = k - 1;
+		b2 = b1 - 1;
 	}
 	
 	/* This function will resolve the parent of b1 b2, then recurr*/
 	if (maps[mapIdx][b2] == 0) {
 		int parent = b1 / 2;
-		freeAndMergeBuddies(maps, mapIndex - 1, parent);
+		freeAndMergeBuddies(maps, mapIdx - 1, parent);
 	}
 }
 
@@ -71,7 +73,7 @@ int bmeminit(int handle, long n_bytes, unsigned int flags, int parm1, int* parm2
 	int i;
 	
 	/* This is the maximum number of pages we can get from n bytes, where parm1 is the minimum size of a page */
-	numPages = n_bytes / parm1;
+	int numPages = n_bytes / parm1;
 	
 	/* if numPages is not a power of two, then there's no way to continue. */
 	if (!isPowerOfTwo(numPages))
@@ -80,7 +82,7 @@ int bmeminit(int handle, long n_bytes, unsigned int flags, int parm1, int* parm2
 	/* calculating number of  Bitmaps we need */
 	int numBitmaps = 0;
 	int tmp;
-	for(tmp = numPages; tmp > 1; tmp >> 1) {
+	for(tmp = numPages; tmp > 1; tmp = tmp >> 1) {
 		numBitmaps += 1;
 	}
 	
@@ -129,7 +131,7 @@ int bmeminit(int handle, long n_bytes, unsigned int flags, int parm1, int* parm2
 	s->minPageSize = parm1;
 	
 	/* we need to know how many bitmaps we have */
-	s->numBitMaps = numBitmaps;
+	s->numBitmaps = numBitmaps;
 	
 	s->firstFree = NULL;
 	
@@ -154,18 +156,10 @@ int bmeminit(int handle, long n_bytes, unsigned int flags, int parm1, int* parm2
 void* bmemalloc(int handler, long n_bytes) {
 	int i;
 	/* 512 is the max number of allocators we can have at once */
-	struct space* s;
-	
-	for (i = 0; i < 512; i++) {
-		if (spaces[i]->handler == handler) {
-			s =  spaces[i];
-			break;
-		}
-		return NULL;
-	}
+	struct space* s = spaces[handler];
 	 
 	int currentBlockSize = s->minPageSize;
-	for (i = s->numBitMaps - 1; i >= 0; i--) {
+	for (i = s->numBitmaps - 1; i >= 0; i--) {
 		/* Find out if n bytes would fit into our current block lvl */
 		if (n_bytes <= currentBlockSize)
 			break;
@@ -175,10 +169,10 @@ void* bmemalloc(int handler, long n_bytes) {
 	}
 	
 	/* local map */
-	int* map = s->bitmaps[i];
+	char* map = s->bitmaps[i];
 	
 	/* +1 because we dont have a map of a single item  */
-	int lengthOfMap = 2 ^ i + 1;
+	int lengthOfMap = 1 << (i + 1);
 	
 	int j;
 	for (j = 0; j < lengthOfMap; j++) {
@@ -219,7 +213,7 @@ void* bmemalloc(int handler, long n_bytes) {
 	/* if we are still here, it means we cant find a free slot at the lowest level,
 	   it is now necessary to recurr and split some blocks up */
 	
-	allocAndMark(n_bytes, s->bitmaps, i-1, j, currentBlockSize * 2);
+	allocAndMark(s, n_bytes, s->bitmaps, i-1, j, currentBlockSize * 2);
 	
 	return NULL;
 }
@@ -234,7 +228,7 @@ for map in bitmaps:
 void bmemfree(void* region) {
 	int i;
 	/* capture the space where region resides */
-	for (i = 0; i < 512; i++) {
+	for (i = 0; i < NUM_SPACES; i++) {
 		if (region > spaces[i]->head && region < spaces[i]->end) {
 			struct space* s = spaces[i];
 			
@@ -243,9 +237,9 @@ void bmemfree(void* region) {
 			
 			int j;
 			/* look from the lowest bitmap first */
-			for (j = s->numBitMaps - 1; j >= 0; j--) {
+			for (j = s->numBitmaps - 1; j >= 0; j--) {
 				// for our bit map, look at each block and see if our region starts there.
-				int* map = s->bitmaps[j];
+				char* map = s->bitmaps[j];
 				int lengthOfMap = 2 ^ j;
 				int k;
 				for (k = 0; k < lengthOfMap -1; k++) {
